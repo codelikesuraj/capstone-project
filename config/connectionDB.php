5494894a8fbbdee9f5972194e1f6306a3b7617ad
abdulbakisurajtest <surajabdulbaki19@gmail.com>
2021-05-09 13:02:52 +0100

    changed pdo connection to mysqli connection

--- config/connectionDB.php
+++ config/connectionDB.php
@@ -13,7 +13,7 @@ session_start();
 
 // instantiate database variables
 $environment = $_SERVER['HTTP_HOST'];
-$host = NULL;
+$servername = NULL;
 $username = NULL;
 $password = NULL;
 $dbname = NULL;
@@ -22,7 +22,7 @@ $local_env = array('localhost', 'LOCALHOST', '127.0.0.1');
 
 //  environment is localhost or 127.0.0.1
 if(in_array($environment, $local_env)):
-    $host = $environment;
+    $servername = $environment;
     $username = 'root';
     $password = '';
     $dbname = 'capstone_project';
@@ -31,19 +31,16 @@ if(in_array($environment, $local_env)):
 else:
     $url = parse_url('mysql://bf669104db9850:be16b333@us-cdbr-east-03.cleardb.co
 	m/heroku_d3bc72f83cb3167?reconnect=true');
-    $host = $url['host'];
+    $servername = $url['host'];
     $username = $url['user'];
     $password = $url['pass'];
     $dbname = substr($url["path"],1);
 endif;
 
-try
-{
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-}
-catch(PDOException $error)
-{
-    die('Error connecting to database('. $dbname.'): '.$error->getMessage());
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn){
+	die("Connection failed: ".mysqli_connect_error());
 }
 
 ?>
